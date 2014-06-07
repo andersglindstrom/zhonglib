2594aed39d5907f7aaf2d38624d692b4481db71a
andersglindstrom <anders@anderslindstrom.com>
2014-06-07 11:21:53 +0800

    Got unit tests working for Unicode characters.

--- test/test_character_decomposition.py
+++ test/test_character_decomposition.py
@@ -19,9 +19,9 @@ class TestCharacterDecomposition(unittest.TestCase):
     _character_t    = ('t', zl.CHARACTER, zl.COMPOSED_OF, [ _group_1, _character_n ])
     _character_u    = ('u', zl.CHARACTER, zl.VARIANT_OF, _character_t )
     _group_2        = ('2', zl.GROUP,     zl.COMPOSED_OF, [ _character_t, _group_1 ])
-    _character_nu   = ('女', zl.CHARACTER, zl.COMPOSED_OF, None)
-    _character_zi   = ('子', zl.CHARACTER, zl.COMPOSED_OF, None)
-    _character_hao  = ('好', zl.CHARACTER, zl.COMPOSED_OF, [_character_nu, _character_zi])
+    _character_nu   = (u'女', zl.CHARACTER, zl.COMPOSED_OF, None)
+    _character_zi   = (u'子', zl.CHARACTER, zl.COMPOSED_OF, None)
+    _character_hao  = (u'好', zl.CHARACTER, zl.COMPOSED_OF, [_character_nu, _character_zi])
 
     @classmethod
     def setUpClass(self):
@@ -29,41 +29,40 @@ class TestCharacterDecomposition(unittest.TestCase):
                                 os.path.dirname(__file__),
                                 'test_decomposition_data.txt')
         self._decomposer = zl.Decomposer(decomp_data_file)
-        #print(self._decomposer)
 
     def decompose(self, ch):
         return self._decomposer.decompose(ch)
 
-    def disable_test_none(self):
+    def test_none(self):
         with self.assertRaises(RuntimeError):
             self.decompose(None)
 
-    def disable_test_level_zero(self):
+    def test_level_zero(self):
         # Test a simple character that has no children components.
         self.assertEqual(self._character_n, self.decompose('n'))
 
-    def disable_test_level_one_character(self):
+    def test_level_one_character(self):
         self.assertEqual(self._character_r, self.decompose('r'))
 
-    def disable_test_level_two_character(self):
+    def test_level_two_character(self):
         self.assertEqual(self._character_s, self.decompose('s'))
 
-    def disable_test_level_one_group(self):
+    def test_level_one_group(self):
         self.assertEqual(self._group_1, self.decompose('1'))
 
-    def disable_test_character_composed_of_group(self):
+    def test_character_composed_of_group(self):
         self.assertEqual(self._character_t, self.decompose('t'))
 
-    def disable_test_variant(self):
+    def test_variant(self):
         self.assertEqual(self._character_u, self.decompose('u'))
 
-    def disable_test_group_with_group(self):
+    def test_group_with_group(self):
         self.assertEqual(self._group_2, self.decompose('2'))
 
-    def disable_test_utf8(self):
+    def test_utf8(self):
         self.assertEqual(self._character_zi, self.decompose(u'子'))
-        #self.assertEqual(self._character_nu, self.decompose('女'))
-        #self.assertEqual(self._character_hao, self.decompose('好'))
+        self.assertEqual(self._character_nu, self.decompose(u'女'))
+        self.assertEqual(self._character_hao, self.decompose(u'好'))
 
 if __name__ == '__main__':
     unittest.main()
