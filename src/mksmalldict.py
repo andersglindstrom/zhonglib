db7693d7d0040f63dafccb42161ae9284447329b
andersglindstrom <anders@anderslindstrom.com>
2014-09-25 13:10:33 +0800

    Added script.

---
+++ src/mksmalldict.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import zhonglib
+
+# Generates a dictionary directory from a cc-cedict file.
+# The resulting dictionary will only be searchable by Mandarin text.
+
+if len(sys.argv) != 3:
+    print 'usage:', sys.argv[0], '<src> <dst>'
+    sys.exit(1)
+
+src = sys.argv[1]
+dst = sys.argv[2]
+
+if os.path.exists(dst):
+    print 'Dictionary already exists.  Remove before generating.'
+    sys.exit(1)
+
+zhonglib.create_dictionary(src, dst, english_index=False, verbose=True)
