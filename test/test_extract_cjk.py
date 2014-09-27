67498589bbf7ca3297ae2b06950c7af83b356c1a
andersglindstrom <anders@anderslindstrom.com>
2014-09-27 17:03:02 +0800

    Minor fix to test case.

---
+++ test/test_extract_cjk.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+import os.path
+import unittest
+import zhonglib as zl
+
+class TestExtractCJK(unittest.TestCase):
+
+    def test_0(self):
+        pattern, words = zl.extract_cjk('')
+        self.assertEqual('', pattern)
+        self.assertEqual((), words)
+
+    def test_1(self):
+        pattern, words = zl.extract_cjk('Hello, world.')
+        self.assertEqual(u'Hello, world.', pattern)
+        self.assertEqual((), words)
+
+    def test_2(self):
+        pattern, words = zl.extract_cjk(u'你好, world.')
+        self.assertEqual(u'%s, world.', pattern)
+        self.assertEqual((u'你好',), words)
+
+    def test_3(self):
+        pattern, words = zl.extract_cjk(u'你好, world. 門口人 to the door.')
+        self.assertEqual(u'%s, world. %s to the door.', pattern)
+        self.assertEqual((u'你好', u'門口人'), words)
+
+    def test_4(self):
+        pattern, words = zl.extract_cjk(u'你好, world. 門口人')
+        self.assertEqual(u'%s, world. %s', pattern)
+        self.assertEqual((u'你好', u'門口人'), words)
