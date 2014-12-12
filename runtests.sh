ec82c691e0bfd213180da999cd7a3c8763fed833
andersglindstrom <anders@anderslindstrom.com>
2014-12-12 19:09:29 +0800

    Add lib/whoosh

--- runtests.sh
+++ runtests.sh
@@ -1,5 +1,5 @@
 echo Running tests in Python 2
-PYTHONPATH=${PWD}/src:${PYTHONPATH} python2 -m unittest discover test/
+PYTHONPATH=${PWD}/src:${PWD}/lib:${PYTHONPATH} python2 -m unittest discover test/
 
 #echo Running tests in Python 3
 #PYTHONPATH=${PWD}/src:${PYTHONPATH} python3.3 -m unittest discover test/
