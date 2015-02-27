1b25ef15b52c6cb9a38f94fa2affe32a4694dc5f
andersglindstrom <anders@anderslindstrom.com>
2015-02-27 22:32:54 +1100

    python3.3 -> python3 in script

--- runtests.sh
+++ runtests.sh
@@ -2,4 +2,4 @@ echo Running tests in Python 2
 PYTHONPATH=${PWD}/src:${PWD}/lib:${PYTHONPATH} python2 -m unittest discover test/
 
 #echo Running tests in Python 3
-#PYTHONPATH=${PWD}/src:${PYTHONPATH} python3.3 -m unittest discover test/
+#PYTHONPATH=${PWD}/src:${PYTHONPATH} python3 -m unittest discover test/
